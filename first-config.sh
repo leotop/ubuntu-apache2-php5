7f87be8d16615bf71ea8c9c7f61c45f952172111
leotop <specialist-seo@yandex.com>
2013-12-01 09:18:31 +0300

    Create first-config.sh

---
+++ first-config.sh
@@ -0,0 +1,4 @@
+# устанавливаем кодировку по умолчанию
+sudo locale-gen ru
+# устанавливаем все необходимое для работы
+sudo apt-get install apache2 php5 libapache2-mod-php5 mysql-server mysql-client mc nano php5-mysql php5-gd phpmyadmin
